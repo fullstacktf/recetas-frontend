dd2806d57a3b78fe4f5ad44cd574f974993b371b
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-08 17:09:28 +0000

    FIX linter error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders hola mundo', () => {
-  render(<App />);
+  render(<App/>);
   const linkElement = screen.getByText(/Hola Mundo/i);
   expect(linkElement).toBeInTheDocument();
 });
