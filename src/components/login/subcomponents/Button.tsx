e7d149b6cdd0bf508433c421b3967e9f600c381c
Pancho Mendoza <alu0100846768@ull.edu.es>
2020-12-11 21:07:24 +0000

    ADD: create prop on Button with primary and secondary style

--- src/components/login/subcomponents/Button.tsx
+++ src/components/login/subcomponents/Button.tsx
@@ -1,15 +1,15 @@
 import React, { FC} from 'react';
 import styled from '@emotion/styled';
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<any>`
   width: 100%;
   text-align: center;
   font-size: 1em;
 
-  color: #FFFFFF;
-  background: #18A0FB;
+  color:  ${(props) => props.secondary ? '#18A0FB;' : '#FFFFFF;'};
+  background: ${(props) => props.secondary ? '#FFFFFF;' : '#18A0FB;'};
   border-radius: 10px;
-  border: 0.5px solid #18A0FB;
+  border: 0.5px solid #18A0FB;;
   box-sizing: border-box;
 
   margin-top: 2em;
@@ -25,21 +25,21 @@ const StyledButton = styled.button`
   }
 
   :active {
-    background: #2ba6f8;
+    background: ${props => props.secondary ? '#dadada;' : '#2ba6f8;'};
   }
 `;
 
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset';
+  secondary?: boolean;
   onClick?: () => void;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
 
   const hanleOnClick = (event: any) => {
-    event.preventDefault();
     props.onClick && props.onClick();
   };
 
-  return <StyledButton type={props.type} onClick={hanleOnClick}>{props.children}</StyledButton>;
+  return <StyledButton secondary={props.secondary} type={props.type} onClick={hanleOnClick}>{props.children}</StyledButton>;
 };
