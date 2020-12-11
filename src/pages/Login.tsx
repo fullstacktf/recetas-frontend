acf605941116c7f483629ea17454a52b9a508fae
Pancho Mendoza <alu0100846768@ull.edu.es>
2020-12-11 21:05:30 +0000

    ADD: Created page for Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,9 @@
-import React, { FC, useState } from 'react';
-import styled from '@emotion/styled';
+import React, { FC } from 'react';
 import { LoginForm } from '../components/login/LoginForm';
-import { Brand } from '../components/header/subcomponents/Brand';
-import { RegisterForm } from '../components/login/RegisterForm';
-
-const Container = styled.div`
-  display: grid;
-  grid-template-rows: 2% 0.6fr 2% repeat(3, 1fr) 2%;
-  grid-template-columns: 5% repeat(3, 1fr) 5% repeat(3, 1fr) 5%;
-  grid-row-gap: 0px;
-  grid-column-gap: 0px;
-`;
-
-const Grid1 = styled.div`
-  grid-area: 2 / 6 / 3 / 9;
-  display: flex;
-`;
-const Grid2 = styled.div`
-  grid-area: 4 / 2 / 7 / 5;
-  padding: 20px;
-  background: url('https://api.snapfork.me/static/mediaUtils/login/background.png');
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center center;
-`;
-const Grid3 = styled.div`
-  grid-area: 4 / 6 / 7 / 9;
-  min-width: 400px;
-  max-width: 500px;
-`;
+import { LoginLayout } from '../layouts/LoginLayout';
 
 export const Login: FC = () => {
-  const [view, setView] = useState('Login');
-
-  const changeView = () => {
-    setView('Register');
-  };
-
-  return <Container>
-    <Grid1><Brand fontSize="70px">SnapFork</Brand></Grid1>
-    <Grid2/>
-    <Grid3>
-      {view === 'Login'
-        ? <LoginForm onSubmit={() => { console.log('Enviando...'); }} goToRegister={changeView}/>
-        : <RegisterForm onSubmit={() => { console.log('Enviando...'); }}/>
-      }
-    </Grid3>
-  </Container>;
+  return <LoginLayout>
+    <LoginForm onSubmit={() => { console.log('Enviando registro...'); }}/>
+  </LoginLayout>;
 };
