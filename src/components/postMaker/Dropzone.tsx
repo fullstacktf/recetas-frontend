0890fac082b55724abd55a2c43546ae9846255b8
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-12 12:09:56 +0000

    ADD a dropzone component

--- src/components/postMaker/Dropzone.tsx
+++ src/components/postMaker/Dropzone.tsx
@@ -1,20 +1,49 @@
-import React, { FC, useCallback } from 'react';
-import {useDropzone} from 'react-dropzone';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export const Dropzone: FC = () => {
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
+  const [image, setImage] = useState<any[]>([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    setImage(
+      acceptedFiles.map((file: any) => {
+      console.log(file);
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        });
+      }
+      )
+    );
   }, []);
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: 'image/*',
+    onDrop
+  });
+
+  useEffect(
+    () => () => {
+      image.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [image]
+  );
+
+  const thumbs = image.map((file) => (
+    <div key={file.name}>
+      <div>
+        <img src={file.preview} alt=""/>
+      </div>
+    </div>
+  ));
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} aria-label="dropzone"/>
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+      <aside>{thumbs}</aside>
     </div>
   );
 };
