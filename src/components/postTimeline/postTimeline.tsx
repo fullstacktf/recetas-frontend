5fb42b1b120c3cb8bd488cbecbd9c6554818d855
Pancho Mendoza <alu0100846768@ull.edu.es>
2020-12-14 17:45:30 +0000

    ADD: initialize PostTimeline component

---
+++ src/components/postTimeline/postTimeline.tsx
@@ -0,0 +1,12 @@
+import { FC} from 'react';
+import styled from '@emotion/styled';
+
+export interface PostTimelineProps{
+
+}
+
+export const PostTimeline: FC<PostTimelineProps> = (props) => {
+
+  return <p>PostTimeline</p>;
+
+};
