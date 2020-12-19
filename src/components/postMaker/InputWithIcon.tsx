58f0c329e1fb832ebbd0f5fa81b513a474e037f8
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-19 19:14:37 +0000

    ADD checking input value

--- src/components/postMaker/InputWithIcon.tsx
+++ src/components/postMaker/InputWithIcon.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 import React, { FC, useState } from 'react';
 
+const ERROR_SIZE = 0.8;
+
 const Input = styled.input`
   display: flex;
   align-items: center;
@@ -34,18 +36,50 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: ${ERROR_SIZE + 0.2 + 'em'};
+  margin: 0px;
+  padding: 5px;
+  padding-left: 10px;
+`;
+
+const ErrorIcon = styled.p`
+  font-size: ${ERROR_SIZE / 2.6 + 'em'};
+  margin: 0px;
+  margin-right: 5px;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-size: ${ERROR_SIZE + 'em'};
+  margin: 0px;
+`;
+
 export interface InputWithIconProps {
   image: string;
   placeHolder: string;
   setValue: (value: string) => void;
+  checkInput: (value: string) => { isValid: boolean; msg: string } ;
 }
 
 export const InputWithIcon: FC<InputWithIconProps> = (props) => {
   const [value, setValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChangeValue = (event: any) => {
-    setValue(event.currentTarget.value);
-    props.setValue(event.currentTarget.value);
+    const check = props.checkInput(event.currentTarget.value);
+    if (check.isValid) {
+      setValue(event.currentTarget.value);
+      props.setValue(event.currentTarget.value);
+      setIsValid(true);
+    } else {
+      setErrorMessage(check.msg);
+      setIsValid(false);
+    }
   };
 
   return (
@@ -58,6 +92,12 @@ export const InputWithIcon: FC<InputWithIconProps> = (props) => {
         aria-label="input-with-icon"
         onChange={handleChangeValue}
       />
+      {!isValid && (
+        <ErrorContainer>
+          <ErrorIcon>{'❌'}</ErrorIcon>
+          <ErrorMessage>{errorMessage}</ErrorMessage>
+        </ErrorContainer>
+      )}
     </Container>
   );
 };
