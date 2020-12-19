dfc6ad0d0ca6796813f95ec15a815f497e756e1c
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-19 18:36:22 +0000

    ADD new test for PostMaker

--- src/components/postMaker/__test__/PostMaker.test.tsx
+++ src/components/postMaker/__test__/PostMaker.test.tsx
@@ -10,6 +10,7 @@ describe('Test PostMaker', () => {
     render(<PostMaker/>);
 
     const droparea = screen.queryByLabelText('dropzone');
+
     expect(droparea).not.toBeNull();
   });
 
@@ -17,6 +18,47 @@ describe('Test PostMaker', () => {
     render(<PostMaker/>);
 
     const titleArea = screen.queryByLabelText('title-input');
+
     expect(titleArea).not.toBeNull();
+  });
+
+  test('should render a input text by placeholder', () => {
+    render(<PostMaker/>);
+
+    const titleArea = screen.queryByPlaceholderText('Título de la receta...');
+
+    expect(titleArea).not.toBeNull();
+  });
+
+  test('should render 6 images', () => {
+    render(<PostMaker/>);
+
+    const images = screen.getAllByRole('img');
+
+    expect(images.length).toBe(6);
+  });
+
+  test('should render 1 button', () => {
+    render(<PostMaker/>);
+
+    const button = screen.getAllByRole('button');
+
+    expect(button.length).toBe(1);
+  });
+
+  test('should render 4 collapse', () => {
+    render(<PostMaker/>);
+
+    const collapse = screen.getAllByLabelText('collapse-div');
+
+    expect(collapse.length).toBe(4);
+  });
+
+  test('should render 4 collapse', () => {
+    render(<PostMaker/>);
+
+    const collapse = screen.getAllByLabelText('collapse-div');
+
+    expect(collapse.length).toBe(4);
   });
 });
