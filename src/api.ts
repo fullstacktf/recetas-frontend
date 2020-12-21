97813f6a96a21d1af2adf14385837e4f7bd4d88d
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-21 21:47:10 +0000

    FIX upload image

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import { user } from './user';
+
 export const API = 'https://api.snapfork.me/';
 export interface FormPost {
   title: string;
@@ -19,6 +21,9 @@ const sendToBackend = async (
   endpoint: string,
   requestOptions: RequestOptions
 ) => {
+  const authorization = { 'Authorization': `Bearer ${localStorage.getItem('token')}`};
+  Object.assign(requestOptions.headers, authorization);
+  console.log(requestOptions);
   return fetch(API + endpoint, requestOptions).then((response) =>
     response.json()
   );
@@ -38,16 +43,23 @@ export const uploadFormData = async (
     .then((data) => data.data)
     .catch((error) => console.log(error));
   if (data) {
-    uploadImage(image);
+    uploadImage(image, user._id, data._id);
   }
 };
 
-export const uploadImage = async (image: File) => {
-  const fd = new FormData();
-  fd.append('image', image);
+export const uploadImage = async (
+  image: File,
+  userID: string,
+  postID: string
+) => {
+  const body = new FormData();
+  body.append('image', image);
+  body.append('userID', userID);
+  body.append('postID', postID);
   const requestOptions = {
     method: 'POST',
-    body: fd
+    body: body,
+    headers: { 'Content-Type': 'multipart/form-data' }
   };
   await sendToBackend('post/upload-image', requestOptions)
     .then((data) => data.data)
@@ -64,7 +76,11 @@ export const getPostData = async (endpoint: string) => {
   return data;
 };
 
-export const updateLike = async (endpoint: string, method: string, body = {userID: '5fddfe4b4d3bf162d342b71d'}) => {
+export const updateLike = async (
+  endpoint: string,
+  method: string,
+  body = { userID: '5fddfe4b4d3bf162d342b71d' }
+) => {
   const requestOptions = {
     method: method,
     body: JSON.stringify(body)
@@ -75,7 +91,11 @@ export const updateLike = async (endpoint: string, method: string, body = {userI
   return data;
 };
 
-export const updateSave = async (endpoint: string, method: string, body = {userID: '5fddfe4b4d3bf162d342b71d'}) => {
+export const updateSave = async (
+  endpoint: string,
+  method: string,
+  body = { userID: '5fddfe4b4d3bf162d342b71d' }
+) => {
   const requestOptions = {
     method: method,
     headers: { 'Content-Type': 'application/json' },
